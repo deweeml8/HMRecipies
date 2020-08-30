2d886a3c976adfa3dd38125d3a4aceb298d448bf
Melanie <deweem@capricorn.com.na>
2020-08-30 14:13:37 +0200

    Adding theming structure

---
+++ HMRecipies/Styling/Themes/Base.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HMRecipies.Styling.Themes
+{
+    public partial class Base : ResourceDictionary
+    {
+        public Base()
+        {
+            InitializeComponent();
+        }
+    }
+}
